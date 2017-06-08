72c68370eec7d2dd6fc2ad5bcddb31a45ffc509f
Matt Settles <mattsettles@gmail.com>
2017-06-08 06:18:55 -0700

    Added error codes for report

--- bin/run_pacbio_rs2_report.sh
+++ bin/run_pacbio_rs2_report.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 ## Pacific Biosciences RSII Report Generation
 ## version 1.0
-
+## Error Codes, 0-sucessfully created reports, 1-error, 2-report already exists, 3-cell exists, needed files do not
 module --silent load R/local_libs
 
 template_rpt="../markdown_reports/pacbio_RSII_report_template_v1.Rmd"
@@ -15,8 +15,20 @@ cell_cell="$3"
 output_dir="${cell_basedir}/${cell_run}/${cell_cell}"
 output_basefilename="PacBioReport-${cell_run}-${cell_cell}"
 
+if [ ! -d "${output_dir}" ]; then
+  exit 1
+fi
+
+# Report already exists
 if [ -f ${output_dir}/${output_basefilename}.pdf ]; then
-   exit 2
+  exit 2
+fi
+
+# Folder does not contain needed files
+metadata=$(find ${output_dir} -maxdepth 1 -mindepth 1 -name '*.metadata.xml')
+csv=$(find ${output_dir}/Analysis_Results -maxdepth 1 -mindepth 1 -name '*.sts.csv')
+if [ ! -f $metadata | ! -f $csv ]; then
+  exit 3
 fi
 
 ## render html report
